400ea3c460cba8bff2f53a0916cc9d27c4f3fa3a
yamikaganji <yamikaganji@gmail.com>
2021-08-03 07:45:49 +0530

    leapyear

--- Leapyear.java
+++ Leapyear.java
@@ -1,9 +1,18 @@
 package PracticePrograms;
 
+import java.util.Scanner;
+
 public class Leapyear {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		 Scanner sc=new Scanner(System.in);
+			System.out.println("Enter a Year");
+			int year = sc.nextInt();
+	                if((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+	                        System.out.println(year + " is a leap year.");
+			else
+				System.out.println(year + " is not a leap year.");
 
 	}
 
