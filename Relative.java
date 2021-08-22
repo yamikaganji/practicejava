b4d11246c375c75321f472f9e8e85c7448ba1f8b
yamikaganji <yamikaganji@gmail.com>
2021-08-22 20:17:03 +0530

    In Chefland, the speed of light is c m/s, and acceleration due to gravity is g m/s2.

---
+++ Relative.java
@@ -0,0 +1,24 @@
+package PracticePrograms;
+
+import java.util.Scanner;
+
+public class Relative {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stubScanner sc = new Scanner(System.in);
+		Scanner sc=new Scanner(System.in);
+		int t = sc.nextInt();
+		for(int i=1;i<=t;i++){
+		    int g =sc.nextInt();
+		    int c = sc.nextInt();
+		    if(g>=1 && g<=10 && c>=1000 && c<=3000){
+		        int H = c*c;
+		        int V = 2*g;
+		        int res = H/V;
+		        System.out.println(res);
+		    }
+		}
+
+	}
+
+}
