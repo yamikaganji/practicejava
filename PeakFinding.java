bfb7bc336a87c14e0ebcb601084a46e9ef9c29fc
yamikaganji <yamikaganji@gmail.com>
2021-08-27 10:17:41 +0530

    code chef Problem Code: UWCOI20A

--- PeakFinding.java
+++ PeakFinding.java
@@ -1,9 +1,25 @@
 package PracticePrograms;
 
+import java.util.Scanner;
+
 public class PeakFinding {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0){
+		    int n=sc.nextInt();
+		    int max=0;
+		    int a;
+		    for(int i=0;i<n;i++){
+		        a=sc.nextInt();
+		        if(a>max){
+		            max=a;
+		        }
+		    }
+		    System.out.println(max);
+		}
 
 	}
 
