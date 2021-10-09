955378edb17ceb83f802048555db5fbbfbb99d83
yamikaganji <yamikaganji@gmail.com>
2021-10-09 22:23:56 +0530

    codechef Problem Code: CHSFORMT

---
+++ Chess.java
@@ -0,0 +1,29 @@
+package PracticePrograms;
+
+import java.util.Scanner;
+
+public class Chess {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int rep = sc.nextInt();
+		for(int i =1; i<=rep; i++) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			if(a+b<3) {
+				System.out.println("1");
+			} else if(3<=a+b && a+b<=10) {
+				System.out.println("2");
+			} else if(11<=a+b && a+b<=60) {
+				System.out.println("3");
+			} else {
+				System.out.println("4");
+			}
+			
+			
+		}
+
+	}
+
+}
