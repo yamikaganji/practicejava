8e543e5bf2f6fcbd41498a34636f671536850dc4
yamikaganji <yamikaganji@gmail.com>
2021-10-23 21:25:23 +0530

    java program to convert a string to uppercase

---
+++ ToUpperCase.java
@@ -0,0 +1,15 @@
+package PracticePrograms;
+
+import java.util.Scanner;
+
+public class ToUpperCase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		String s=sc.next();
+		System.out.println(s.toUpperCase());
+
+	}
+
+}
