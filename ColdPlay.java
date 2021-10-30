a00d3748c056a6ab4006effada26752c872afe33
yamikaganji <yamikaganji@gmail.com>
2021-10-30 21:36:13 +0530

    CodeChef Problem Code: CHFRICH

--- ColdPlay.java
+++ ColdPlay.java
@@ -1,9 +1,27 @@
 package PracticePrograms;
 
+import java.util.Scanner;
+
 public class ColdPlay {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		 try{
+				Scanner sc = new Scanner(System.in);
+				int n = sc.nextInt();
+				for(int x=1;x<=n;x++)
+				{
+				   
+				    int M = sc.nextInt();
+				    int S = sc.nextInt();
+				    int t = (M/S);
+				    
+				    System.out.println(t);
+				     
+				 }      
+				}catch(Exception e){
+				return;
+				}
 
 	}
 
---
+++ RichrRich.java
@@ -0,0 +1,10 @@
+package PracticePrograms;
+
+public class RichrRich {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
