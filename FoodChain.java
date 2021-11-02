9cd090385f2efd12b01185043bb3645089aca1e1
yamikaganji <yamikaganji@gmail.com>
2021-11-02 21:02:11 +0530

    Code Chef ProblemCode :FODCHAIN

--- FoodChain.java
+++ FoodChain.java
@@ -1,9 +1,25 @@
 package PracticePrograms;
 
+import java.util.Scanner;
+
 public class FoodChain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0)
+		{
+		    int e=sc.nextInt();
+		    int k=sc.nextInt();
+		    int res=0;
+		    while(e!=0)
+		    {
+		        res++;
+		        e=e/k;
+		    }
+		    System.out.println(res);
+		}
 
 	}
 
