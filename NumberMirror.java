bf9e36597e2e82d48ca077947b9ca0b4dae8ff52
yamikaganji <yamikaganji@gmail.com>
2021-11-09 21:16:56 +0530

    CodeChef Problem Code: START01

---
+++ NumberMirror.java
@@ -0,0 +1,14 @@
+package PracticePrograms;
+
+import java.util.Scanner;
+
+public class NumberMirror {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		System.out.println(n);
+	}
+
+}
